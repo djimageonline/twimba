2f4b451dfca5cacaa7d8df82e0a2dbc95108c474
Johnny Proano <jproano22@yahoo.com>
2023-03-28 22:23:04 -0400

    removed TweetBTN variable and added another else if searching for the button click and created handle tweetbtn click

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import { tweetsData } from "/data.js";
 
 const tweetInput = document.getElementById("tweet-input");
-const tweetBtn = document.getElementById("tweet-btn");
-
-tweetBtn.addEventListener("click", function () {
-  console.log(tweetInput.value);
-});
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.like) {
@@ -14,6 +9,8 @@ document.addEventListener("click", function (e) {
     handleRetweetClick(e.target.dataset.retweet);
   } else if (e.target.dataset.reply) {
     handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
   }
 });
 
@@ -47,6 +44,10 @@ function handleRetweetClick(tweetId) {
 
 function handleReplyClick(replyId) {
   document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
+}
+
+function handleTweetBtnClick() {
+  console.log(tweetInput.value);
 }
 
 function getFeedHtml() {
